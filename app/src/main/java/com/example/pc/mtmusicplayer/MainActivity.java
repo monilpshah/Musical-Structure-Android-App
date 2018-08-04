9bef60ff8d7361fe236bf7d6f6a45222c17a54d9
monilpshah <monil31199@gmail.com>
2018-08-04 21:10:59 +0530

    drawable plugins

--- app/src/main/java/com/example/pc/mtmusicplayer/MainActivity.java
+++ app/src/main/java/com/example/pc/mtmusicplayer/MainActivity.java
@@ -2,12 +2,11 @@ package com.example.pc.mtmusicplayer;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,7 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     MediaPlayer mediaPlayer;
     int pausecurrentposition;
 
-    @Override
+    @Override   
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -43,26 +42,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         stop.setOnClickListener(this);
 
     }
-    final View.OnClickListener playlistsTextViewOnClickListener = new View.OnClickListener(){
-        public void onClick(final View v){
+
+    final View.OnClickListener playlistsTextViewOnClickListener = new View.OnClickListener() {
+        public void onClick(final View v) {
             openPlaylistsList(playlistsCardView);
         }
     };
 
-    final View.OnClickListener playerTextViewOnClickListener = new View.OnClickListener(){
-        public void onClick(final View v){
+    final View.OnClickListener playerTextViewOnClickListener = new View.OnClickListener() {
+        public void onClick(final View v) {
             openPlayer(playerCardView);
         }
     };
 
-    final View.OnClickListener albumsTextViewOnClickListener = new View.OnClickListener(){
-        public void onClick(final View v){
+    final View.OnClickListener albumsTextViewOnClickListener = new View.OnClickListener() {
+        public void onClick(final View v) {
             openAlbumsList(albumsCardView);
         }
     };
 
-    final View.OnClickListener settingsTextViewOnClickListener = new View.OnClickListener(){
-        public void onClick(final View v){
+    final View.OnClickListener settingsTextViewOnClickListener = new View.OnClickListener() {
+        public void onClick(final View v) {
             openSettingsList(settingsCardView);
         }
     };
@@ -86,7 +86,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent intent = new Intent(this, Setting.class);
         startActivity(intent);
     }
-
 
 
     public void onClick(View view) {
